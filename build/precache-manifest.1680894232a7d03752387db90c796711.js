60ad2c6bb2201f2d412f245216e4197cce3714f9
Davon Wilson <davontech3@gmail.com>
2024-04-22 19:33:28 -0400

    made adjustemts for formatting

---
+++ build/precache-manifest.1680894232a7d03752387db90c796711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001ab8ad11712fbe41b1731e2e29d344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0345adb84816130e56",
+    "url": "/static/js/2.96f04768.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.96f04768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4751b82d7b2c949883d",
+    "url": "/static/js/main.9739791f.chunk.js"
+  },
+  {
+    "revision": "6265c6ba9acdeaf37436",
+    "url": "/static/js/runtime-main.52c7198b.js"
+  }
+]);
